a3dadf02b7dbab1f734a991ac4a246f88df5f7b2
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-05-04 09:50:36 +0300

    warning fix

--- src/main/java/ru/mail/polis/DAOFactory.java
+++ src/main/java/ru/mail/polis/DAOFactory.java
@@ -53,6 +53,6 @@ public final class DAOFactory {
         if (!data.isDirectory()) {
             throw new IllegalArgumentException("Path is not a directory: " + data);
         }
-        return new NewDAO(data, MAX_HEAP / 128);
+        return new NewDAO(data, MAX_HEAP / 50);
     }
 }
